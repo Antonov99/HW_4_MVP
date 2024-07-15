5236c7def1a12dd96f759cc7e38f8092d432c625
Aleksey <lexa.antonov-alex@yandex.ru>
2024-07-15 23:02:49 +0300

    .

--- Assets/Scripts/Helper/PopupHelper.cs
+++ Assets/Scripts/Helper/PopupHelper.cs
@@ -6,27 +6,49 @@ namespace Lessons.Architecture.PM
 {
     public sealed class PopupHelper : MonoBehaviour
     {
-        [FormerlySerializedAs("_characterPopup")] [SerializeField] private CharacterPopupView characterPopupView;
-        [SerializeField] private StatsView _statsView;
-        [SerializeField] private UserView _userView;
-        
-        [SerializeField] private PopupInfo _popupInfo;
-        
+        [FormerlySerializedAs("_characterPopup")]
+        [SerializeField]
+        private CharacterPopupView characterPopupView;
+
+        [SerializeField]
+        private StatsView _statsView;
+
+        [SerializeField]
+        private UserView _userView;
+
+        [SerializeField]
+        private PopupInfo _popupInfo;
+
         private PopupModel _popupModel;
         private PopupPresenterFactory _popupPresenterFactory;
-        
-        [SerializeField] private string newName;
-        [SerializeField] private string newDescription;
-        [SerializeField] private Sprite newIcon;
-        [SerializeField] private int expToAdd;
-        
-        [SerializeField] private string nameToAdd;
-        [SerializeField] private int valueToAdd;
-        [SerializeField] private string nameToRemove;
-        
-        [SerializeField] private string nameToChange;
-        [SerializeField] private int valueToChange;
-        
+
+        [SerializeField]
+        private string newName;
+
+        [SerializeField]
+        private string newDescription;
+
+        [SerializeField]
+        private Sprite newIcon;
+
+        [SerializeField]
+        private int expToAdd;
+
+        [SerializeField]
+        private string nameToAdd;
+
+        [SerializeField]
+        private int valueToAdd;
+
+        [SerializeField]
+        private string nameToRemove;
+
+        [SerializeField]
+        private string nameToChange;
+
+        [SerializeField]
+        private int valueToChange;
+
         [Inject]
         private void Construct(PopupPresenterFactory popupPresenterFactory)
         {
@@ -37,9 +59,10 @@ namespace Lessons.Architecture.PM
         public void ShowPopup()
         {
             var popupPresenter = _popupPresenterFactory.CreatePopupPresenter();
-            var userPresenter = _popupPresenterFactory.CreateUserPresenter(_popupModel.UserInfo, _popupModel.PlayerLevel);
+            var userPresenter =
+                _popupPresenterFactory.CreateUserPresenter(_popupModel.UserInfo, _popupModel.PlayerLevel);
             var statsPresenter = _popupPresenterFactory.CreateStatsPresenter(_popupModel.CharacterInfo);
-            
+
             characterPopupView.Show(popupPresenter);
             _userView.Show(userPresenter);
             _statsView.Show(statsPresenter);
@@ -59,7 +82,7 @@ namespace Lessons.Architecture.PM
         {
             _popupModel.UserInfo.ChangeDescription(newDescription);
         }
-        
+
         public void ChangeIcon()
         {
             _popupModel.UserInfo.ChangeIcon(newIcon);
@@ -67,16 +90,16 @@ namespace Lessons.Architecture.PM
 
         public void RemoveStat()
         {
-            var stat=_popupModel.CharacterInfo.GetStat(nameToRemove);
+            var stat = _popupModel.CharacterInfo.GetStat(nameToRemove);
             _popupModel.CharacterInfo.RemoveStat(stat);
-            
+
             ShowPopup();
         }
 
         public void AddStat()
         {
             if (_popupModel.CharacterInfo.GetStats().Length >= 6) return;
-            
+
             CharacterStat characterStat = new CharacterStat();
             characterStat.Name = nameToAdd;
             characterStat.ChangeValue(valueToAdd);
